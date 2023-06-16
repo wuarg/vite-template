d5f22549d6726bb209875390f46288ec089ca66e
g g <gg@guangdeMacBook-Pro.local>
2023-06-16 13:45:58 +0800

    init:iost

--- src/views/swap/template.vue
+++ src/views/swap/template.vue
@@ -4,7 +4,7 @@
 <script lang="ts">
   import { defineComponent, ref, onMounted } from 'vue';
   export default defineComponent({
-    name: '',
+    name: 'TemplateVue',
     setup() {
       onMounted(() => {
         // connectWith(connectors[0]);
