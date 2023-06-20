09566decca1652af639b3d149b45d7fc8b71dbe5
g g <gg@guangdeMacBook-Pro.local>
2023-06-20 11:29:53 +0800

    feat:数据板块

---
+++ src/layout/footer copy.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import { isDark, useThemeChang } from '~/composables';
+  import { useI18n } from '~/modules/i18n';
+  const { t, toggleLocale } = useI18n();
+
+  const { themeAni } = useThemeChang(isDark);
+  const str = '</>';
+</script>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'LayoutFooter',
+  });
+</script>
+<template>
+  <div class="my-10 text-center text-lg">
+    <router-link to="/about" class="mx-4" active-class="font-normal">
+      <span>
+        <sup class="sub">{{ str }}</sup> {{ $t('about.link_name') }}</span
+      >
+    </router-link>
+    <router-link to="/home" class="mx-4" active-class="font-normal">
+      <span
+        ><sup>{{ str }}</sup> {{ $t('home') }}</span
+      >
+    </router-link>
+  </div>
+</template>
+<style lang="less" scoped>
+  .nav-btn {
+    @apply mx-2 cursor-pointer text-blue;
+  }
+</style>
--- src/layout/footer.vue
+++ src/layout/footer.vue
@@ -1,33 +1,18 @@
-<script setup lang="ts">
-  import { isDark, useThemeChang } from '~/composables';
-  import { useI18n } from '~/modules/i18n';
-  const { t, toggleLocale } = useI18n();
-
-  const { themeAni } = useThemeChang(isDark);
-  const str = '</>';
-</script>
+<template>
+  <div class="fixed inset-x-0 bottom-0 my-10 text-center text-lg"> {{ account }} </div>
+</template>
 <script lang="ts">
   import { defineComponent } from 'vue';
+  import { commonStore } from '~/stores/modules/common';
   export default defineComponent({
     name: 'LayoutFooter',
+    setup() {
+      const appStore = commonStore();
+      const account = appStore.getAccount;
+      return {
+        account,
+      };
+    },
   });
 </script>
-<template>
-  <div class="my-10 text-center text-lg">
-    <router-link to="/about" class="mx-4" active-class="font-normal">
-      <span>
-        <sup class="sub">{{ str }}</sup> {{ $t('about.link_name') }}</span
-      >
-    </router-link>
-    <router-link to="/home" class="mx-4" active-class="font-normal">
-      <span
-        ><sup>{{ str }}</sup> {{ $t('home') }}</span
-      >
-    </router-link>
-  </div>
-</template>
-<style lang="less" scoped>
-  .nav-btn {
-    @apply mx-2 cursor-pointer text-blue;
-  }
-</style>
+<style lang="less" scoped></style>
