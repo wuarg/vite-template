e1a99ae700b397304069c4a888dfb8b7d7b94baf
g g <gg@guangdeMacBook-Pro.local>
2023-12-02 14:56:43 +0800

    feat: 添加基本组件

---
+++ src/components/core/Input.vue
@@ -0,0 +1,56 @@
+<template>
+  <input
+    type="text"
+    :class="`app-input size-${size}`"
+    v-bind="$attrs"
+    :value="modelValue"
+    @input="inputHandler"
+  />
+</template>
+
+<script>
+  export default {
+    name: 'BaseInput',
+    props: {
+      modelValue: {
+        type: String,
+      },
+      size: {
+        type: String,
+        default: 'medium',
+        validator(value) {
+          return ['medium', 'large'].includes(value);
+        },
+      },
+    },
+    methods: {
+      inputHandler(e) {
+        this.$emit('update:modelValue', e.target.value);
+      },
+    },
+  };
+</script>
+
+<style lang="less" scoped>
+  @import '~/style/variables.less';
+  .app-input {
+    width: 100%;
+    background: none;
+    font-size: 16px;
+    padding: 5px 12px;
+    border-radius: 20px;
+    border: 1px solid @darkBorderSecondaryColor;
+    background-color: @darkSecondaryBackground;
+    box-sizing: border-box;
+
+    &.size-large {
+      font-size: 18px;
+      padding: 16px;
+    }
+    &,
+    &:focus {
+      outline: none;
+      color: #fff;
+    }
+  }
+</style>
---
+++ src/components/core/Tooltip.vue
@@ -0,0 +1,90 @@
+<template>
+  <span
+    ref="tooltip-holder"
+    class="tooltip-holder"
+    @mouseenter="mouseenterHandler"
+    @mouseleave="mouseleaveHandler"
+  >
+    <slot />
+  </span>
+  <p
+    :class="['tooltip-content', { show: showTooltip }]"
+    :style="{
+      top: `${tooltipHolderPosition.top + tooltipHolderPosition.height}px`,
+      left: `${tooltipHolderPosition.left + tooltipHolderPosition.width / 2}px`,
+    }"
+  >
+    {{ text }}
+  </p>
+</template>
+
+<script>
+  export default {
+    name: 'Tooltip',
+    props: {
+      text: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        tooltipHolderPosition: {},
+        showTooltip: false,
+      };
+    },
+    methods: {
+      mouseenterHandler() {
+        this.updateTooltipHolderPosition();
+        this.showTooltip = true;
+      },
+      mouseleaveHandler() {
+        this.showTooltip = false;
+      },
+      updateTooltipHolderPosition() {
+        this.tooltipHolderPosition = this.$refs['tooltip-holder'].getBoundingClientRect();
+      },
+    },
+  };
+</script>
+
+<style lang="less" scoped>
+  @import '~/style/variables.less';
+  .tooltip-holder {
+    cursor: default;
+  }
+  .tooltip-content {
+    position: fixed;
+    transform: translateX(-50%);
+
+    max-width: 220px;
+    display: none;
+
+    font-size: 16px;
+    padding: 10px 16px;
+    border-radius: 12px;
+    background-color: @darkBackground;
+    color: @grayText;
+    font-weight: 400;
+    border: 1px solid @darkBorderColor;
+    z-index: 1;
+    &:before,
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: 100%;
+      left: 50%;
+      transform: translateX(-50%);
+      border-bottom: 9px solid @darkBackground;
+      border-right: 9px solid transparent;
+      border-left: 9px solid transparent;
+    }
+    &:before {
+      border-bottom-color: @darkBorderColor;
+      border-width: 10px;
+    }
+    &.show {
+      display: block;
+    }
+  }
+</style>
